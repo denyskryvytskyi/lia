2a43418b2ec696c3e4d814414b39c6dc67efd1cb
Denys Kryvytskyi <kryvytskyi.denys@gmail.com>
2020-11-21 16:30:10 +0200

    Removed extra cpp file

--- gdm/mat4.h
+++ gdm/mat4.h
@@ -25,7 +25,25 @@ namespace gdm
         /**
         * Constructs an identity matrix.
         */
-        mat4();
+        mat4()
+        {
+            m[0][0] = 1.0f;
+            m[0][1] = 0.0f;
+            m[0][2] = 0.0f;
+            m[0][3] = 0.0f;
+            m[1][0] = 0.0f;
+            m[1][1] = 1.0f;
+            m[1][2] = 0.0f;
+            m[1][3] = 0.0f;
+            m[2][0] = 0.0f;
+            m[2][1] = 0.0f;
+            m[2][2] = 1.0f;
+            m[2][3] = 0.0f;
+            m[3][0] = 0.0f;
+            m[3][1] = 0.0f;
+            m[3][2] = 0.0f;
+            m[3][3] = 1.0f;
+        }
 
         /**
         * Constructs a matrix initialized to the specified value.
@@ -50,7 +68,25 @@ namespace gdm
         mat4(float m00, float m01, float m02, float m03,
             float m10, float m11, float m12, float m13,
             float m20, float m21, float m22, float m23,
-            float m30, float m31, float m32, float m33);
+            float m30, float m31, float m32, float m33)
+        {
+            m[0][0] = m00;
+            m[0][1] = m01;
+            m[0][2] = m02;
+            m[0][3] = m03;
+            m[1][0] = m10;
+            m[1][1] = m11;
+            m[1][2] = m12;
+            m[1][3] = m13;
+            m[2][0] = m20;
+            m[2][1] = m21;
+            m[2][2] = m22;
+            m[2][3] = m23;
+            m[3][0] = m30;
+            m[3][1] = m31;
+            m[3][2] = m32;
+            m[3][3] = m33;
+        }
 
         /**
         * Constructs a matrix initialized by the vectors.
@@ -60,14 +96,50 @@ namespace gdm
         * @param row3 The z unit basis vector
         * @param row4 The translation vector
         */
-        mat4(vec4 row1, vec4 row2, vec4 row3, vec4 row4);
+        mat4(vec4 row1, vec4 row2, vec4 row3, vec4 row4)
+        {
+            m[0][0] = row1.x;
+            m[0][1] = row1.y;
+            m[0][2] = row1.z;
+            m[0][3] = row1.w;
+            m[1][0] = row2.x;
+            m[1][1] = row2.y;
+            m[1][2] = row2.z;
+            m[1][3] = row2.w;
+            m[2][0] = row3.x;
+            m[2][1] = row3.y;
+            m[2][2] = row3.z;
+            m[2][3] = row3.w;
+            m[3][0] = row4.x;
+            m[3][1] = row4.y;
+            m[3][2] = row4.z;
+            m[3][3] = row4.w;
+        }
 
         /**
          * Constructs an matrix with scalar by diagonal.
          *
          * @param scalar Diagonal scalar value
          */
-        mat4(float scalar);
+        mat4(float scalar)
+        {
+            m[0][0] = scalar;
+            m[0][1] = 0.0f;
+            m[0][2] = 0.0f;
+            m[0][3] = 0.0f;
+            m[1][0] = 0.0f;
+            m[1][1] = scalar;
+            m[1][2] = 0.0f;
+            m[1][3] = 0.0f;
+            m[2][0] = 0.0f;
+            m[2][1] = 0.0f;
+            m[2][2] = scalar;
+            m[2][3] = 0.0f;
+            m[3][0] = 0.0f;
+            m[3][1] = 0.0f;
+            m[3][2] = 0.0f;
+            m[3][3] = scalar;
+        }
 
         float& operator()(int i, int j)
         {
@@ -102,7 +174,7 @@ namespace gdm
             return vec4(m[index][0], m[index][1], m[index][2], m[index][3]);
         }
 
-        const float* ElementsPtr()
+        const float* ElementsPtr() const
         {
             return &(m[0][0]);
         }
